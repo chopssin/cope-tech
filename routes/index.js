facb539c8c84062a044eb598df89f9756bde723d
Chopssin <chopssin@gmail.com>
2016-12-06 11:00:45 +0800

    debug req.hostname in router.get("*")

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('*', function(req, res, next) {
-  debug(req.originalUrl);
+  debug('hostname = ' + req.hostname);
   next();
 });
 

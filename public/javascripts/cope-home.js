ffa2d131ba9af0793f1abcc5991a6dcb52463e37
Chopssin <chopssin@gmail.com>
2016-12-12 01:41:22 +0800

    empty file

--- public/javascripts/cope-home.js
+++ public/javascripts/cope-home.js
@@ -1,19 +1,3 @@
 (function($) {
 
-var Cope = {};
-Cope.signIn = function() {
-  var _thenable = {}, done;
-  _thenable.then = function(_cb) {
-    done = _cb;
-  };
-  // TBD: get config and then sign in
-  // done.call(Cope);
-  return _thenable;
-};
-
-// Prompt sign-in panel
-Cope.signIn().then(function() { 
-  console.log(this); 
-});
-
 })(jQuery);

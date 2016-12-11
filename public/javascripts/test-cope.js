2415419215533b4c155c4e794d62136b1f266732
Chopssin <chopssin@gmail.com>
2016-12-12 01:42:15 +0800

    apply the rewritten "setTest" to test functions

--- public/javascripts/test-cope.js
+++ public/javascripts/test-cope.js
@@ -1,78 +1,93 @@
 (function($) {
-var tests = {},
-    autoTests = [], passed = 0;
 
-// Set auto tests
-autoTests.push(util.setTest('Create a test app', function() {
+var test = util.setTest('test-cope');
 
-  return this.done();
+// Test - graphDB
+test(function(pass) {
+  
+  var debug = this.debug,
+      G = graphDB();
+  
+  // Test - create new app with appId = "testApp2"
+  test(function(pass) {
+    G.createApp('testApp2').then(function(_appG) {
+      if (_appG) {
+        pass('End of app creation test');
+        return;
+      } 
+      pass('End of app creation test');
 
-  var capp = copeApp(),
-      done = this.done,
-      debug = this.debug;
-  capp.create('testApp').then(function(_app) {
-    debug(_app); 
-    done();
-  });
-}));
-autoTests.push(util.setTest('Check jQuery', function() {
-  var done = this.done,
-      debug = this.debug;
-  if ($) {
-    done();
-  } else {
-    debug('undefined jQuery or $');
-  }
-}));
-autoTests.push(util.setTest('Build test - testApp', function() {
-  var capp = copeApp('testApp'),
-      GDB = capp.useGraphDB,
-      done = this.done,
-      debug = this.debug;
-
-  // Set page "testPage"
-  capp.setPage('testPage', function() {
-    var G = GDB(), 
-        fetchDS = dataSnap();
-    fetchDS.load(function() {
-      try {
-        var testCol = G.col('testCol'), no_1;
-        no_1 = testCol.add('no_1');
-        no_1.val({
-          publishedAt: new Date().getTime()
-        });
-        no_1.val('publishedAt').then(function(val) {
-          var time = util.timeOf(val);
-          if (!time) {
-            debug('util.timeOf(publishedAt)', time);
+      // Test - get app by appId = "testApp2"
+      test(function(pass) {
+        G.getApp('testApp2').then(function(_appG2) {
+          if (!_appG2) {
+            debug('testApp2', 'Failed to getApp');
+            return;
           } 
-          done();
-        });
-      } catch (err) { console.error(err); }
-    });
-    fetchDS.load();
-  });
+          pass('Get app "testApp2"');
 
-  // Build page "testPage"
-  capp.build({ 
-    //editable: true, 
-    //path: '/test/path', 
-    page: 'testPage' 
-  });
-})); // end of test
+          var dreams = _appG2.col('dreams'),
+              dreamers = _appG2.col('dreamers'),
+              dreamedBys = _appG2.edges('dreamedBys');
+          var daydream = dreams.node('daydream');
+          var jones = dreamers.node('jones');
 
-// Run auto tests
-autoTests.forEach(function(test) {
-  test.run(function() {
-    var msg = this.msg || '';
-    passed += 1;
-    console.log('Passed: ' + passed + '/' + autoTests.length + ' ' + msg);
-    if (passed == autoTests.length) {
-      console.log('=================');
-      console.log('All tests done.');
-      console.log('=================');
-    }
-  });
+          // Test - set values of nodes
+          test(function(pass) {
+            daydream.val({
+              happiness: 98
+            });
+      
+            jones.val({
+              name: 'Jones'
+            });
+
+            pass("Set values of nodes");
+          }); 
+
+          // Test - set edges
+          test(function(pass) {
+            dreamedBys.add(daydream, jones);
+        
+            dreamedBys.from(daydream).then(function(_nodes) {
+              pass('Set an edge, and queried via "#from"');
+              
+              if (!Array.isArray(_nodes)) debug('testApp2', '_nodes is not an array');
+
+              _nodes.forEach(function(_n) {
+                test(function(pass) {
+                  dreamedBys.del(daydream, _n).then(function() {
+                    pass('deleted the edge');
+                  });
+                }).print();
+              });
+
+              test(function(pass) { 
+                _nodes[0].val().then(function(val) {
+                  if (!val) {
+                    debug('Failed to find _nodes[0]');
+                    return;
+                  }
+                  pass('Found _nodes[0]');
+                });
+              }).print();
+            }); // end of dreamedBys.from
+          }); // end of test
+        }); // end of G.getApp
+      }); // end of test
+    }); // end of G.createApp
+  }); // end of test
+
+  pass('Start test');
+}); // end of test
+
+// Test - use jQuery
+test(function(pass) {
+  if ($) {
+    pass('$ is defined');
+  } else {
+    this.debug('undefined jQuery or $');
+  }
 });
 
 })(jQuery)

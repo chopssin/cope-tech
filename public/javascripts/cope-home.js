516291f473fe7143baa1aecaf05580f56edb427b
Chopssin <chopssin@gmail.com>
2016-12-12 21:30:19 +0800

    Navigation of "mypage"

--- public/javascripts/cope-home.js
+++ public/javascripts/cope-home.js
@@ -1,3 +1,25 @@
 (function($) {
 
+// Navigation
+var nav = function(_sec) {
+  switch(_sec) {
+    case 'home':
+      $('#dashboard').removeClass('hidden');
+      $('#dashboard-app').addClass('hidden');
+      break;
+    case 'app':
+      $('#dashboard').addClass('hidden');
+      $('#dashboard-app').removeClass('hidden');
+      break;
+  }
+};
+
+$('#logo').click(function() {
+  nav('home');
+});
+
+$('#sample').click(function() {
+  nav('app');
+});
+
 })(jQuery);

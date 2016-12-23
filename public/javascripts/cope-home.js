3ae11ba83d8e766ab2e3c24a80b9ce1991b22f28
Chopssin <chopssin@gmail.com>
2016-12-23 14:15:56 +0800

    separate "ViewDataGraph" from "ViewAppPage"

--- public/javascripts/cope-home.js
+++ public/javascripts/cope-home.js
@@ -8,6 +8,7 @@ var G = Cope.graphDB(),
     renderDS = dataSnap(),
     ViewAppCard = Views.class('AppCard'),
     ViewAppPage = Views.class('AppPage'),
+    ViewDataGraph = Views.class('DataGraph'),
     nav,
     debug = util.setDebug('cope-home', true);
 
@@ -28,6 +29,7 @@ $('#logo').click(function() {
   nav('home');
 });
 
+// Stage of fetching data
 fetchDS.load(function() {
   G.listMyApps().then(function(_res) {
     var count = 0, myOwnApps = [];
@@ -50,12 +52,13 @@ fetchDS.load(function() {
   });
 }); 
 
+// Stage of rendering views
 renderDS.load(function() {
   var myOwnApps = this.val().myOwnApps;
   $('#my-apps').html('');
   
   myOwnApps.forEach(function(_a) {
-    ViewAppCard.build({
+    var vuCard = ViewAppCard.build({
       sel: '#my-apps',
       method: 'append',
       data: {
@@ -64,18 +67,37 @@ renderDS.load(function() {
         appName: _a.appName
       }
     }).res('touched', function(_data) {
+      // App being selected
+      // Navigate to app section
       nav('app');
-      ViewAppPage.build({
-        sel: '#app-page',
-        data: { appCard: this }
-      });
-    }); // AppCard
-  });
-});
 
+      // Get the initial graph
+      d3.json('d3-sample.json', function(err, graph) {
+        if (err) throw err;
+
+        // Build app section
+        ViewAppPage.build({
+          sel: '#app-page',
+          data: { 
+            appCard: vuCard,
+            graph: graph
+          }
+        }).res('selected', function(node) {
+          // TBD: Node being selected
+          // Show node's value
+          debug('selected node', node);
+        });
+      }); // end of getting initial graph
+    }); // end of AppCard
+  }); // end of myOwnApps.forEach
+}); // end of renderDS.load
+
+// All start from here
 fetchDS.load();
 
-// Views
+// Create Views
+
+// "AppCard"  
 ViewAppCard.dom(function() {
   return '<div' + this._ID + ' class="cope-card touchable bg-w" style="margin-bottom:16px">'
     + '<h3 data-component="appName"></h3>'
@@ -98,8 +120,10 @@ ViewAppCard.render(function() {
   that.$el().off('click').on('click', function() {
     that.res('touched', that.val());
   });
-});
+});  
+// end of "AppCard"
 
+// "AppPage"
 ViewAppPage.dom(function() {
   return '<div' + this._ID + 'class="row">' 
     + '<div class="col-xs-12">' 
@@ -132,7 +156,8 @@ ViewAppPage.dom(function() {
     + '</div>'
     //+ '<div class="col-xs-12"><svg width="960" height="600"></svg></div>'
   + '</div>';
-});
+}); // end of ViewAppPage.dom
+
 ViewAppPage.render(function() {
   var appCard = this.val('appCard'),
       val = appCard.val(),
@@ -157,13 +182,25 @@ ViewAppPage.render(function() {
   // val.owner
   // val.partners
   // val.expiredAt
-});
+}); // end of ViewAppPage.render
+
 ViewAppPage.render(function() { // draw the graph
-  
-  var $card = this.$el('@card'),
+  var appCard = this.val('appCard'),
+      graph = this.val('graph');
+      $card = this.$el('@card'),
       $svgWrap = this.$el('@svg'),
-      w = $svgWrap.width();
-  $svgWrap.html('<svg width="' + w + '" height="600"></svg>');
+      w = $svgWrap.width(),
+
+
+  Views.class('DataGraph').build({
+    sel: this.sel('@svg'),
+    data: {
+      width: w,
+      height: 600,
+      graph: graph
+    }
+  });
+
   $card.css('z-index', 0);
   $svgWrap.css('z-index', 1);
 
@@ -180,10 +217,31 @@ ViewAppPage.render(function() { // draw the graph
       .css('z-index', 0);
   });
 
-  var svg = d3.select("svg"),
+}); // end of ViewAppPage.render // draw the graph
+// end of "AppPage"
+
+// DataGraph
+// properties:
+// - width
+// - height
+// - appId
+ViewDataGraph.dom(function() {
+  var w = this.val('width'), 
+      h = this.val('height');
+  return '<svg' + this._ID + 'id="svg" width="'+ w +'" height="' + h + '"></svg>';
+});
+
+ViewDataGraph.render(function() {
+  var graph = this.val('graph');
+
+  debug('ViewDataGraph - graph', graph);
+
+  var svg = d3.select("svg#svg"),
       width = +svg.attr("width"),
       height = +svg.attr("height");
-
+  
+  if (!graph || !width || !height) return;
+  
   var color = d3.scaleOrdinal(d3.schemeCategory20);
 
   var simulation = d3.forceSimulation()
@@ -191,55 +249,53 @@ ViewAppPage.render(function() { // draw the graph
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(width / 2, height / 2));
 
-  d3.json("d3-sample.json", function(error, graph) {
-    if (error) throw error;
+  var link = svg.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(graph.links)
+      .enter().append("line")
+      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-    var link = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(graph.links)
-        .enter().append("line")
-        .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+  var node = svg.append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(graph.nodes)
+      .enter().append("circle")
+      .attr("r", 5)
+      .attr("fill", function(d) { return color(d.group); })
+      .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
 
-    var node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(graph.nodes)
-        .enter().append("circle")
-        .attr("r", 5)
-        .attr("fill", function(d) { return color(d.group); })
-        .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+  node.append("title")
+    .text(function(d) { return d.id; });
 
-    node.append("title")
-        .text(function(d) { return d.id; });
+  simulation
+    .nodes(graph.nodes)
+    .on("tick", ticked);
 
-    simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+  simulation.force("link")
+    .links(graph.links);
 
-    simulation.force("link")
-      .links(graph.links);
+  function ticked() {
+    link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
-    function ticked() {
-      link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-      node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-    }
-  });
+    node
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+  }
 
   function dragstarted(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
+
+    console.log(d);
   }
 
   function dragged(d) {
@@ -252,7 +308,7 @@ ViewAppPage.render(function() { // draw the graph
     d.fx = null;
     d.fy = null;
   }
-
-}); // end of ViewAppPage.render // draw the graph
+}); // end of ViewDataGraph.render
+// end of "DataGraph"
 
 })(jQuery);
